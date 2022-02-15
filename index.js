67c2560458dbdf50738c632500fe2520a390ad9f
Steven Sinha <stevensinha329@gmail.com>
2022-02-15 02:26:38 -0500

    create dynamic site based on pug template

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const pug = require('pug');
+const parser = require('body-parser')
+const app = express();
+const port = 3003;
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.use(parser.urlencoded( { extended: true}))
+app.use(express.static('./'));
+
+app.get("/ttt/", function(req, res) {
+    res.render('index', {name: ""});
+});
+
+app.post("/ttt/", function(req, res) {
+    res.render('index', {name: req.body.name});
+});
+
+app.listen(port, ()=> {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
