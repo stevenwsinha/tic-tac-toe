34f28c3fe4ca99bd902314e3ba0c1284ae1428f5
Steven Sinha <stevensinha329@gmail.com>
2022-02-15 11:22:55 -0500

    add client side ajax requests

---
+++ ttt/client.js
@@ -0,0 +1,33 @@
+const { request } = require("express");
+
+var httpRequest;
+var board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+const url = './ttt/play'
+
+function makeRequest() {
+    httpRequest = new XMLHttpRequest();
+
+    // make a random move 
+    var index = Math.floor(Math.random() * 9); 
+    board[index] = 'X';
+
+    let requestJson = {};
+    requestJson.grid = board;
+
+    httpRequest.onreadystatechange = responseHandler;
+    httpRequest.open('POST', url);
+    httpRequest.setRequestHeader('Content-Type', 'application/json');
+    httpRequest.send(requestJson);
+}
+
+function responseHandler(){
+    if(httpRequest.readyState === XMLHttpRequest.DONE) {
+        if(httpRequest.status === 200) {
+            var responseJson = JSON.parse(httpRequest.responseText);
+            board = responseJson.grid;
+            if(responseJson.winner !== ' '){
+                alert(`Winner: ${responseJson.winner}!`);
+            }
+        }
+    }
+}
