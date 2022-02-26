31863c91883d51c06a90441505396ce316518d74
Steven Sinha <stevensinha329@gmail.com>
2022-02-25 19:54:32 -0500

    Add checks for game end to front end

--- ttt/client.js
+++ ttt/client.js
@@ -1,24 +1,36 @@
 var httpRequest;
 var board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+var winner
 const url = './play'
 
+// make a move locally, then send a request to update the server
 function makeMoveRequest(move) {
-    httpRequest = new XMLHttpRequest();
-
+    // get the button element that was clicked on 
     button = move.target
-
-    // change the button
-    button.innerHTML=('X')
 
     // get the index of button clicked on 
     index = parseInt(button.id.substring("button-".length));
 
+    // only allow moves on open squares, and if the game hasn't ended
+    if(board[index] !== ' ' || winner){
+        return;
+    }
+
+    // create new request
+    httpRequest = new XMLHttpRequest();
+
+    // change the button's symbol
+    button.innerHTML=('X')
+
+    // change the board
     board[index] = 'X'
 
+    // create the data to be sent
     let requestJson = {};
     requestJson.grid = board;
-
     data = JSON.stringify(requestJson);
+    
+    // send the request, wait for response, then call responseHandler()
     httpRequest.onreadystatechange = responseHandler;
     httpRequest.open('POST', url);
     httpRequest.setRequestHeader('Content-Type', 'application/json');
@@ -28,23 +40,33 @@ function makeMoveRequest(move) {
 function responseHandler(){
     if(httpRequest.readyState === XMLHttpRequest.DONE) {
         if(httpRequest.status === 200) {
-            console.log(httpRequest.responseText);
+            // set our local board to include the server move
             var responseJson = JSON.parse(httpRequest.responseText);
             board = responseJson.grid;
+
+            // update the board to include the server move
             updateView();
+
+            // if there's a winner, send an alert
+            if(responseJson.winner){
+                
+            }
             if(responseJson.winner !== ' '){
                 alert(`Winner: ${responseJson.winner}!`);
+                winner = responseJson.winner
             }
         }
     }
 }
 
+// set the innerhtml to the correct value
 function updateView() {
     for(let i = 0; i < 9; i++){
         document.getElementById('button-' + i).innerHTML = (board[i] == ' ') ? '-' : board[i] 
     }
 }
 
+// attach the make request function to each button
 window.onload = ()=> {
     for(let i = 0; i < 9; i++){
         document.getElementById('button-' + i).addEventListener('click', makeMoveRequest)
